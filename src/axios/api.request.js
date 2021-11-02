8181c40dcf3665dd51fd2afe72274ebf21278bf3
PY <181250111@smail.nju.edu.cn>
2021-11-02 17:56:15 +0800

    请求携带Cookies

--- src/axios/api.request.js
+++ src/axios/api.request.js
@@ -4,7 +4,8 @@ const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8080'
 
 const HttpRequest = axios.create({
   baseURL: baseUrl,  // api的base_url
-  timeout: 30000  // 请求超时时间
+  timeout: 30000,  // 请求超时时间
+  withCredentials: true
 });
 
 export default HttpRequest;
