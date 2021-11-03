e75834d1d581f069d2085f50f66de8734387d2ba
PY <181250111@smail.nju.edu.cn>
2021-11-04 03:08:26 +0800

    主页新增最近内化法规表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -18,6 +18,63 @@
         <div id="year-line" class="chart"></div>
       </el-card>
     </el-col>
+    <el-col :span="24">
+      <el-divider></el-divider>
+      <h4 style="text-align: left;margin-bottom: 0">&nbsp;&nbsp;最近内化法规</h4>
+      <el-table
+        ref="multipleTable"
+        :data="paperList"
+        v-loading="loading"
+        tooltip-effect="dark"
+        style="width: 100%;margin-top: 10px">
+        <el-table-column
+          label="法规标题">
+          <template slot-scope="scope">{{ scope.row.title }}</template>
+        </el-table-column>
+        <el-table-column
+          prop="department"
+          label="发文机构"
+          width="200">
+        </el-table-column>
+        <el-table-column
+          prop="release_time"
+          label="发布日期"
+          width="120"
+          header-align="center"
+          align="center">
+          <template slot-scope="scope">
+            {{ scope.row.release_time.substring(0, 10) }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="implement_time"
+          label="实施日期"
+          width="120"
+          header-align="center"
+          align="center">
+          <template slot-scope="scope">
+            {{ scope.row.implement_time.substring(0, 10) }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="grade"
+          label="效力等级"
+          width="120"
+          header-align="center"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="status"
+          label="状态"
+          width="80"
+          header-align="center"
+          align="center">
+          <template slot-scope="scope">
+            <el-tag :type="scope.row.status ? 'success' : 'warning'">{{ scope.row.status ? '已发布' : '未发布' }}</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-col>
   </el-row>
 </template>
 
@@ -49,7 +106,10 @@ echarts.use([
 export default {
   name: "Home",
   data() {
-    return {}
+    return {
+      paperList: [],
+      loading: false,
+    }
   },
   mounted() {
     this.getStatisticalData().then(res => {
@@ -122,10 +182,19 @@ export default {
     }).catch(err => {
       this.$message.error(err);
     })
+    this.loading = true;
+    this.getRecentAnalyzedPapers().then(res => {
+      this.paperList = res;
+    }).catch(err => {
+      this.$message.error(err);
+    }).finally(() => {
+      this.loading = false;
+    })
   },
   methods: {
     ...mapActions([
       'getStatisticalData',
+      'getRecentAnalyzedPapers',
     ]),
   },
 }
