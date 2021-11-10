eb6c8ac839ae109c2e63413dc6a6c0253743e446
PY <181250111@smail.nju.edu.cn>
2021-11-11 04:05:32 +0800

    push test

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import router from './router'
 import store from './store'
 
 import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
 import './theme/index.css';
 
 import './permission'; // permission control
